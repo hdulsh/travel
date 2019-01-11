696de6081a622105103933d7fbceea3d57151930
李美丽 <304489471@qq.com>
2019-01-11 12:04:35 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list">
+    <list class="item">A</list>
+    <list class="item">A</list>
+    <list class="item">A</list>
+    <list class="item">A</list>
+    <list class="item">A</list>
+    <list class="item">A</list>
+    <list class="item">A</list>
+  </ul>
+</template>
+
+<script>
+  export default{
+    name:'CityAlphabet'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl";
+  .list
+    display:flex
+    flex-direction:column
+    justify-content:center
+    position:absolute
+    right:0
+    top:1.58rem
+    bottom:0
+    width:.4rem
+    .item
+      text-align:center
+      line-height:.4rem
+      color:$bgColor
+</style>
